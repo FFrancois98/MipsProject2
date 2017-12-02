84aebae19aa016e7836a8464ad2c49da8da0d6ce
FFrancois98 <33648640+FFrancois98@users.noreply.github.com>
2017-12-01 23:50:20 -0500

    Update mipsproject2.asm
    main loop for checking characters

--- mipsproject2.asm
+++ mipsproject2.asm
@@ -21,3 +21,23 @@ main:
 	la $s3, userSubString				#Copy of userSubString to know where to start printing
 	and $t8, $t8, $zero				#Flag when non-terminating char/space has been read
 	and $t9, $t9, $zero				#Current string length counter (8 max length)
+	
+loop:
+	lb $s0, 0($s2)					#Load character into $s0
+	
+	slti $t1, $t9, 9				#Check if current substring is longer than 8 characters
+	beq $t1, $zero, length_error	#Throw length_error and skip to next comma
+	
+	beq $s0, $zero, print_strings	#Check if at end of input
+	beq $s0, '\n', print_strings	#Check if at end of input
+	beq $s0, ',', subStringProcess		#Process chars at the end of the current substring
+	beq $s0, ' ', space_loop
+	
+	li $t8, 1						#Set seen valid character flag
+	sb $s0, 0($s1)					#Save character in current string
+	addi $s2, $s2, 1				#Go to next character from input
+	addi $s1, $s1, 1				#Go to next empty place in userSubString
+	addi $t9, $t9, 1				#Increment current substring length counter (max 8)
+	
+	j loop							#Continue loop
+	
