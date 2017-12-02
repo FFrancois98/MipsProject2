6e96ae88cbaec144bc4af2d2deacefc53399741c
FFrancois98 <33648640+FFrancois98@users.noreply.github.com>
2017-12-01 23:49:26 -0500

    Update mipsproject2.asm
    sets up substring register

--- mipsproject2.asm
+++ mipsproject2.asm
@@ -15,3 +15,9 @@ main:
 	la $a0, userInput				#Store string in buffer
 	li $a1, 1001					#Limit size to 1000
 	syscall
+	
+	la $s1, userSubString				#Save address of userSubString in $s1
+	la $s2, ($a0)					#Move address of input string to $s2
+	la $s3, userSubString				#Copy of userSubString to know where to start printing
+	and $t8, $t8, $zero				#Flag when non-terminating char/space has been read
+	and $t9, $t9, $zero				#Current string length counter (8 max length)
